beb233b65aa58d20702c168c879eecc847b85cd5
AragornOfKebroyd <86985645+AragornOfKebroyd@users.noreply.github.com>
2022-05-01 20:43:11 +0100

    Add files via upload

---
+++ final.py
@@ -0,0 +1,316 @@
+#.........................................................................................................................................................................................................................................................
+#.TTTTTTTTTTThhhh....................................... iii......................................................................................... iii..................ttt..thhh.......hiii...........................................dddd............
+#.TTTTTTTTTTThhhh....................................... iii......................................................................................... iii................. ttt..thhh.......hiii...........................................dddd............
+#.TTTTTTTTTTThhhh......................................................................................................................................................... ttt..thhh......................................................dddd............
+#....TTTT....hhhhhhhh....eeeeee..eerrrrrr.eeeeee........ iii.issssss.........aaaaaa...annnnnnn.........eeeeee..eerrrrrrrrrrrrr..oooooo..oorrrrrr..... iii.innnnnnn...... tttttthhhhhhh...hiii.issssss.........cccccc.....oooooo....ddddddddd..eeeeee....
+#....TTTT....hhhhhhhhh..eeeeeeee.eerrrrrrreeeeeee....... iiiiisssssss....... aaaaaaa..annnnnnnn....... eeeeeee.eerrrrrrrrrrrrrroooooooo.oorrrrrr..... iii.innnnnnnn..... tttttthhhhhhhh..hiiiiisssssss....... ccccccc..coooooooo..oddddddddd.deeeeeee...
+#....TTTT....hhhh.hhhhhheee.eeee.eerrr..rree.eeee....... iiiiiss.ssss...... aa.aaaaa.annn.nnnnn..... ee.eeee.eerrr..rrrrr...rooo.ooooooorrr........ iii.innn.nnnnn...... ttt..thhh.hhhhh.hiiiiiss.ssss...... ccc.cccc.cooo.oooooooddd.dddddddee.eeee...
+#....TTTT....hhhh..hhhhheee..eeeeeerr...rree..eeee...... iiiiisss.............aaaaaa.annn..nnnn..... ee..eeeeeerr...rrrr...rroo...oooooorr......... iii.innn..nnnn...... ttt..thhh..hhhh.hiiiiisss.......... cc..ccc.ccoo...oooooodd...ddddddee..eeee..
+#....TTTT....hhhh..hhhhheeeeeeeeeeerr...rreeeeeeee...... iii.isssss........ aaaaaaaa.annn..nnnn..... eeeeeeeeeerr...rrrr...rroo...oooooorr......... iii.innn..nnnn...... ttt..thhh..hhhh.hiii.isssss........ cc......ccoo...oooooodd...ddddddeeeeeeee..
+#....TTTT....hhhh..hhhhheeeeeeeeeeerr...rreeeeeeee...... iii..sssssss...... aaaaaaaa.annn..nnnn..... eeeeeeeeeerr...rrrr...rroo...oooooorr......... iii.innn..nnnn...... ttt..thhh..hhhh.hiii..sssssss...... cc......ccoo...oooooodd...ddddddeeeeeeee..
+#....TTTT....hhhh..hhhhheee......eerr...rree............ iii......ssss..... aa.aaaaa.annn..nnnn..... ee......eerr...rrrr...rroo...oooooorr......... iii.innn..nnnn...... ttt..thhh..hhhh.hiii......ssss..... cc..ccc.ccoo...oooooodd...ddddddee........
+#....TTTT....hhhh..hhhhheee..eeeeeerr...rree..eeee...... iiiiiss..ssss..... aa.aaaaa.annn..nnnn..... ee..eeeeeerr...rrrr....rooo.ooooooorr......... iii.innn..nnnn...... ttt..thhh..hhhh.hiiiiiss..ssss..... ccc.cccc.cooo.oooooooddd.dddddddee..eeee..
+#....TTTT....hhhh..hhhh.eeeeeeee.eerr....reeeeeee....... iiiiisssssss...... aaaaaaaa.annn..nnnn...... eeeeeeeeerr...rrrr....roooooooo...rr......... iii.innn..nnnn...... tttttthhh..hhhh.hiiiiisssssss....... cccccccc.coooooooo..oddddddddd.deeeeeee...
+#....TTTT....hhhh..hhhh..eeeeee..eerr.....eeeeee........ iii..ssssss........ aaaaaaaa.annn..nnnn.......eeeeeeeerr...rrrr.....oooooo.....rr......... iii.innn..nnnn...... tttttthhh..hhhh.hiii..ssssss.........cccccc.....oooooo....ddddddddd..eeeeee....
+#.........................................................................................................................................................................................................................................................
+
+#*multiple, not 1
+
+#.................................................................................................................................................................................
+#.....GGGGGGG................................dddd......llll.....................kkkk.............ffffiiiii..................dddd.iiii............................iiii..ttt..!!!!..
+#...GGGGGGGGGG...............................dddd......llll.....................kkkk............fffffiiiii..................dddd.iiii............................iiii.tttt..!!!!..
+#..GGGGGGGGGGGG..............................dddd......llll.....................kkkk............ffff........................dddd......................................tttt..!!!!..
+#..GGGGG..GGGGG....oooooo.....oooooo....ddddddddd......lllluuuu..uuuuu..cccccc..kkkk..kkkk..... fffffiiiii.nnnnnnnn....ddddddddd.iiii.nnnnnnnn....ggggggggg......iiiitttttt!!!!!..
+#.GGGGG....GGG...ooooooooo..ooooooooo..dddddddddd......lllluuuu..uuuuu.cccccccc.kkkk.kkkk...... fffffiiiii.nnnnnnnnn..dddddddddd.iiii.nnnnnnnnn..gggggggggg......iiiitttttt!!!!!..
+#.GGGG...........oooo.ooooo.oooo.oooooddddd.ddddd......lllluuuu..uuuuuccccc.cccckkkkkkkk........ffff..iiii.nnnn.nnnnnddddd.ddddd.iiii.nnnn.nnnnnggggg.ggggg......iiii.tttt..!!!!..
+#.GGGG..GGGGGGGGGooo...oooooooo...oooodddd...dddd......lllluuuu..uuuuucccc..ccc.kkkkkkk.........ffff..iiii.nnnn..nnnndddd...dddd.iiii.nnnn..nnnngggg...gggg......iiii.tttt..!!!...
+#.GGGG..GGGGGGGGGooo...oooooooo...oooodddd...dddd......lllluuuu..uuuuucccc......kkkkkkkk........ffff..iiii.nnnn..nnnndddd...dddd.iiii.nnnn..nnnngggg...gggg......iiii.tttt..!!!...
+#.GGGGG.GGGGGGGGGooo...oooooooo...oooodddd...dddd......lllluuuu..uuuuucccc......kkkkkkkk........ffff..iiii.nnnn..nnnndddd...dddd.iiii.nnnn..nnnngggg...gggg......iiii.tttt..!!!...
+#..GGGGG....GGGGGooo...oooooooo...oooodddd...dddd......lllluuuu..uuuuucccc..ccc.kkkkkkkkk.......ffff..iiii.nnnn..nnnndddd...dddd.iiii.nnnn..nnnngggg...gggg......iiii.tttt........
+#..GGGGGGGGGGGG..oooo.ooooo.oooo.oooooddddd.ddddd......lllluuuuu.uuuuuccccc.cccckkkk.kkkk.......ffff..iiii.nnnn..nnnnddddd.ddddd.iiii.nnnn..nnnnggggg.ggggg......iiii.tttt..!!!!..
+#...GGGGGGGGGG...ooooooooo..ooooooooo..dddddddddd......llll.uuuuuuuuuu.ccccccccckkkk..kkkk......ffff..iiii.nnnn..nnnn.dddddddddd.iiii.nnnn..nnnn.gggggggggg......iiii.ttttt!!!!!..
+#.....GGGGGGG......oooooo.....oooooo....ddddddddd......llll..uuuuuuuuu..cccccc..kkkk..kkkk......ffff..iiii.nnnn..nnnn..ddddddddd.iiii.nnnn..nnnn..ggggggggg......iiii.ttttt!!!!!..
+#......................................................................................................................................................gggg.......................
+#...............................................................................................................................................ggggg.gggg........................
+#................................................................................................................................................ggggggggg........................
+#.................................................................................................................................................ggggggg.........................
+#.................................................................................................................................................................................
+
+
+#Imports
+import os as cat
+import sys as dog
+import qrcode as fish
+import numpy as infant
+import json as yourMother
+import random as plane
+import math as meth
+import requests as holyGhostInit
+from cryptography.fernet import Fernet
+import pandas as owo
+import struct as struct
+
+#config files
+config = {
+    "testing protocol" : True,
+    "resolution" : "800x800",
+    "language" : "C#",
+    "site mapping" : True,
+    "OOP" : True,
+    "working" : False,
+    "bouce rate" : 12,
+    "VM version num" : 2.2788b,
+    "Canfigurate" : "Configurator",
+    }
+
+
+
+class MainFrame():
+    def __init__(self,*args,**kwargs):
+        "fishAlgoriothm".__init__(self,*args,**kwargs)
+        self.impV = args[0]
+        self.discrtee = args[1]
+        qrCODE = self.sidelinesetup(self.impV)
+        self.a = range(255)
+        n = (0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             1, 1, 0, 0, 0, 0, 0, 0, 3, 0, 0, 2, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+             0, 0, 0, 0, 0, 0)
+        d = { x: y for x, y in zip(self.a,n) }
+        self.theOnlyTruePath(qrCODE)
+        l = ''.join([chr(c) *n for c,n in d.items()])
+        z = [6,5,0,7,11,1,2,3,4,8,9]
+        o = [0] * 13
+        for c in l:
+            i = z.pop(0)
+            o[i] = c
+            
+    def sidelinesetup(self,importantVariable):
+        self.key = b'D1cFiWXeSv_ZWpyXsUGNusF7USzd_VhtRPdIFELkqZs='
+        fernet = Fernet(self.key)
+        string = b'gAAAAABibrU7XV9DQrlY4W_SA3JfoC6iqH5SaW_It-SEx6CF48UnLtIoBWkYVdNWDmIulKJ1OJ28aGdIPYSNcxkV0nUaeZ5w9h4JTjn-6bjyQ1dt7Zts1ezhexBUuXKc0DqaXUsEavoHiCgmed54A0RCe3wjkkEJDw=='
+        mainFrame = ""
+        for randVar in range(meth.floor(meth.prod([meth.pi,meth.e,69]))):
+            binaries = [0,1]
+            owoify = ''.join(str(binaries[int(bin(int(hex(int(round(plane.random(),0))),16)),2)]) for i in range(plane.randint(5,69)))
+            prit(owoify)
+
+        decMessage = fernet.decrypt(string).decode()
+        return decmessage
+
+    
+    def theOnlyTruePath(self,zipBomb):
+
+        theHighGround = fish.make(zipBomb)
+        theLowGround = ''.join(str(self.key[plane.randint(0,len(self.key)-1)]for jamesMay in range(len(self.a))))
+
+        if True:
+            pass
+            #the highgorund = wins
+
+        theHighGround.save(slef.discrtee)
+
+    def thisisathing()
+        roundConstants = ['428a2f98', '71374491', 'b5c0fbcf', 'e9b5dba5', '3956c25b', '59f111f1', '923f82a4', 'ab1c5ed5','d807aa98', '12835b01', '243185be', '550c7dc3', '72be5d74', '80deb1fe', '9bdc06a7', 'c19bf174','e49b69c1', 'efbe4786', '0fc19dc6', '240ca1cc', '2de92c6f', '4a7484aa', '5cb0a9dc', '76f988da','983e5152', 'a831c66d', 'b00327c8', 'bf597fc7', 'c6e00bf3', 'd5a79147', '06ca6351', '14292967','27b70a85', '2e1b2138', '4d2c6dfc', '53380d13', '650a7354', '766a0abb', '81c2c92e', '92722c85','a2bfe8a1', 'a81a664b', 'c24b8b70', 'c76c51a3', 'd192e819', 'd6990624', 'f40e3585', '106aa070','19a4c116', '1e376c08', '2748774c', '34b0bcb5', '391c0cb3', '4ed8aa4a', '5b9cca4f', '682e6ff3','748f82ee', '78a5636f', '84c87814', '8cc70208', '90befffa', 'a4506ceb', 'bef9a3f7', 'c67178f2']
+            
+        def strToBin(string): #turns string to binary
+            binary = ""
+            for x in string:
+                binary = binary + (format(ord(x), 'b'))
+            return binary
+
+
+        def rightRotate(string, num):
+            output_list = []
+            list1 = list(str(string))
+            num = num % len(list1)
+            # Will add values from n to the new list
+            for item in range(len(list1) - num, len(list1)):
+                output_list.append(list1[item])
+                
+            # n to the end of new list    
+            for item in range(0, len(list1) - num): 
+                output_list.append(list1[item])
+
+            return "".join(output_list)
+
+
+        def rightShift(string,num):
+            for x in range(num):
+                string = "0" + string
+
+            string = string[0:32]
+            return string
+
+
+        def chunkloop(chunk,hash1,hash2,hash3,hash4,hash5,hash6,hash7,hash8):
+            #splits chunk into 16 32 bit segments
+            words = []
+            for x in range(16):
+                words.append(chunk[32*x:32*x+32])
+
+            #add 48 more empty words
+            for x in range(48):
+                words.append('00000000000000000000000000000000')
+
+            #apply an algorithm to these zeroed words
+            for i in range(16,64):
+                s0 = int(rightRotate(words[i - 15], 7),2) ^ int(rightRotate(words[i - 15], 18),2) ^ int(rightShift(words[i - 15], 3),2)
+                s1 = int(rightRotate(words[i- 2], 17),2) ^ int(rightRotate(words[i- 2],19),2) ^ int(rightShift(words[i- 2],10),2)
+                added = int(words[i-16],2) + s0 + int(words[i-7],2) + s1
+                stringed = str(bin(added))[2:]
+                truncated = stringed[0:32]
+                words[i] = truncated
+
+            #Compresion, modifiying a-h
+            a = int(hash1,16)
+            b = int(hash2,16)
+            c = int(hash3,16)
+            d = int(hash4,16)
+            e = int(hash5,16)
+            f = int(hash6,16)
+            g = int(hash7,16)
+            h = int(hash8,16)
+            for i in range(0,64):
+                #messes around with values
+                s1 = int(str(bin(int(rightRotate(e,6),16) ^ int(rightRotate(e,11),16) ^ int(rightRotate(e,25),16)))[2:34],2)
+                temp0 = int(str(e + f)[0:32]) ^ abs(int(str(~e + g)[0:32]))
+                temp1 = h + s1 + temp0 + int(roundConstants[i],16) + int(words[i],2)
+                s0 = int(rightRotate(a,2),16) ^ int(rightRotate(a,13),16) ^ int(rightRotate(a,22),16)
+                major = a + b ^ c + d ^ e + f
+                temp2 = int(str(bin(s0 + major))[2:34],2)
+                h = g
+                g = f
+                f = e
+                e = d + temp1
+                d = c
+                c = b
+                b = a
+                a = int(str(bin(temp1 + temp2))[0:32],2)
+            #hex values
+            hash1 = '{:x}'.format(int(str(bin(int(hash1,16) + a))[2:34],2))
+            hash2 = '{:x}'.format(int(str(bin(int(hash1,16) + b))[2:34],2))
+            hash3 = '{:x}'.format(int(str(bin(int(hash1,16) + c))[2:34],2))
+            hash4 = '{:x}'.format(int(str(bin(int(hash1,16) + d))[2:34],2))
+            hash5 = '{:x}'.format(int(str(bin(int(hash1,16) + e))[2:34],2))
+            hash6 = '{:x}'.format(int(str(bin(int(hash1,16) + f))[2:34],2))
+            hash7 = '{:x}'.format(int(str(bin(int(hash1,16) + g))[2:34],2))
+            hash8 = '{:x}'.format(int(str(bin(int(hash1,16) + h))[2:34],2))
+            #returns the hex hash values so they can be used in the next chunckloop
+            return [hash1,hash2,hash3,hash4,hash5,hash6,hash7,hash8]
+
+        def BHASH68(string):
+            #check if string
+            if type(string) != str:
+                return "please use a string as paramater"
+
+            #Convert string to binary
+            binary = strToBin(string)
+
+            #defines
+            binlen = len(binary)
+            #preset hash values
+            hash1 = "6a09e667"
+            hash2 = "bb67ae85"
+            hash3 = "3c6ef372"
+            hash4 = "a54ff53a"
+            hash5 = "510e527f"
+            hash6 = "9b05688c"
+            hash7 = "1f83d9ab"
+            hash8 = "5be0cd19"
+
+            
+            #append 1
+            binary += "1"
+            
+            #Make a multiple of 512 - 64
+            length = len(binary)
+            while (length + 64) % 512 != 0:
+                binary += "0"
+                length = len(binary)
+                
+            #Big endian
+            bigEndianList = (struct.pack('>I', binlen)) #this fuction returns a list
+            bigEndianInt = 0
+            for x in bigEndianList:
+                bigEndianInt += x #so we combine it to a single int
+            bigEndianBin = "{0:b}".format(bigEndianInt) #transform it to binary
+            bigEndianLen = len(str(bigEndianBin))
+            bigEndian = str(bigEndianBin) #then a string
+            while bigEndianLen % 64 != 0: #and pad it with 0's untill it is 64 bits
+                bigEndian = "0" + bigEndian
+                bigEndianLen = len(str(bigEndian))
+            binary += bigEndian
+
+            #split up into 512 bit chunks
+            chunks = []
+            loops = len(binary) // 512
+            for x in range(loops):
+                chunks.append(binary[512*x:512*x+512])
+
+            #Chunkloop performed on 512 bit increments
+            for chunk in chunks:
+                newhashes = chunkloop(chunk,hash1,hash2,hash3,hash4,hash5,hash6,hash7,hash8)
+                hash1 = newhashes[0]
+                hash2 = newhashes[1]
+                hash3 = newhashes[2]
+                hash4 = newhashes[3]
+                hash5 = newhashes[4]
+                hash6 = newhashes[5]
+                hash7 = newhashes[6]
+                hash8 = newhashes[7]
+            #return final hash
+            return hash1+hash2+hash3+hash4+hash5+hash6+hash7+hash8
+
+
+def coacerate(insata):
+    return
+    dictionary = dict(zip(plain, encryp)) 
+    newmessage = ''
+    for char in text: 
+        try: 
+            newmessage += dictionary[char] 
+        except: 
+            newmessage += ' '
+    self.coacerate()
+    print (text + '\nhas been encrypted to:') 
+    print (newmessage) 
+    
+
+def MainSetupProcess(startKey,*args,**kwargs):
+    startup = startKey
+    for x in config:
+        x.__init__(*args,**kwargs)
+        process = True
+
+        for value in range(len(config)):
+            coacerate(x)
+
+    tempVar = MainFrame("uwu","img")
+    return tempVar
+        
+    
+if __name__ == "__main__":
+    def start(var1):
+        if var1:
+            return MainSetupProcess(turnOn)
+    turnOn = False
+    
+    veryimportant = turnOn
+    
+    getMainFrame = start(veryimportant)
+    if getMainFrame == None:
+        print("well get to debugging then")
+    print(getMainFrame)
+    print("""
+                haxing complete
+                    0x0 xx
+                      gg    
+        """)
